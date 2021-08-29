2d6b04a9137a6e0fa28855c114ec73c593f8192f
Rob Bransby <rob@bransby.com.au>
2021-08-29 15:34:16 +0800

    First pass on MeterReadProcessingService

--- MeterReadingsManagementSystem/Server/Services/MeterReadProcessingService.cs
+++ MeterReadingsManagementSystem/Server/Services/MeterReadProcessingService.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using MeterReadingsManagementSystem.Server.Data;
+using MeterReadingsManagementSystem.Shared;
+using MeterReadingsManagementSystem.Shared.Csv;
+using MeterReadingsManagementSystem.Shared.Model;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -7,5 +11,77 @@ namespace MeterReadingsManagementSystem.Server.Services
 {
     public class MeterReadProcessingService
     {
+        private DataContext _dataContext;
+
+        public MeterReadProcessingService(DataContext dataContext)
+        {
+            _dataContext = dataContext;
+        }
+
+        public IEnumerable<MeterReadProcessingResult> ProcessMeterReadEntries(IEnumerable<MeterReadingEntry> entries)
+        {
+            var results = new List<MeterReadProcessingResult>();
+            foreach (var entry in entries)
+            {
+                var meterReading = new MeterReading();
+                var result = new MeterReadProcessingResult(entry);
+                // first lets try to parse everything to see if we're dealing with valid data
+                if (long.TryParse(entry.AccountId, out long accountId))
+                {
+                    meterReading.AccountId = accountId;
+                }
+                else result.FailReasons.Add(MeterReadingUploadFailReason.InvalidAccountId);
+                
+
+                if (DateTime.TryParse(entry.MeterReadingDateTime, out DateTime dateTime))
+                {
+                    meterReading.MeterReadingDateTime = dateTime;
+                }
+                else result.FailReasons.Add(MeterReadingUploadFailReason.InvalidReadingDate);
+
+                if (int.TryParse(entry.MeterReadValue, out int value))
+                {
+                    meterReading.MeterReadValue = value;
+                }
+                else result.FailReasons.Add(MeterReadingUploadFailReason.InvalidReadValue);
+
+                // if we have any errors at this point, no point continuing, move to the next entry
+                if (result.FailReasons.Any())
+                {
+                    results.Add(result);
+                    continue;
+                }
+                
+                // now validate other business logic
+
+                // reading values should be NNNNN format - < 99999.. although not explicitly stated in the Acceptance criteria, im also assuming negative readings are invalid. Potentially need to clarify
+                if (meterReading.MeterReadValue > 99999 || meterReading.MeterReadValue < 0)
+                {
+                    result.FailReasons.Add(MeterReadingUploadFailReason.InvalidReadValue);
+                }
+
+                // accountId must exist in Accounts
+                if (!_dataContext.Accounts.Any(a => a.Id == meterReading.AccountId))
+                {
+                    result.FailReasons.Add(MeterReadingUploadFailReason.InvalidAccountId);
+                }
+
+                if (_dataContext.MeterReadings.Any(mr => mr.AccountId == meterReading.AccountId && mr.MeterReadingDateTime == meterReading.MeterReadingDateTime))
+                {
+                    result.FailReasons.Add(MeterReadingUploadFailReason.DuplicateMeterReadEntry);
+                }
+
+                if (result.FailReasons.Any())
+                {
+                    results.Add(result);
+                    continue;
+                }
+
+                _dataContext.MeterReadings.Add(meterReading);
+                results.Add(result);
+            }
+
+            return results;
+        }
     }
 }
--- MeterReadingsManagementSystem/Shared/Model/MeterReadProcessingResult.cs
+++ MeterReadingsManagementSystem/Shared/Model/MeterReadProcessingResult.cs
@@ -3,10 +3,26 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using MeterReadingsManagementSystem.Shared.Csv;
 
 namespace MeterReadingsManagementSystem.Shared.Model
 {
-    class MeterReadProcessingResult
+    public class MeterReadProcessingResult : MeterReadingEntry
     {
+        public bool WasSuccessful 
+        { 
+            get 
+            {
+                return FailReasons.Any();
+            } 
+        }                
+        public List<MeterReadingUploadFailReason> FailReasons { get; set; } = new List<MeterReadingUploadFailReason>();
+
+        public MeterReadProcessingResult(MeterReadingEntry entry)
+        {
+            this.AccountId = entry.AccountId;
+            this.MeterReadingDateTime = entry.MeterReadingDateTime;
+            this.MeterReadValue = entry.MeterReadValue;
+        }
     }
 }
